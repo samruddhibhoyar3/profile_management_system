a465781c41605cbaf7a594b32fc5a7c1087b499e
Samruddhi Bhoyar <52337446+samruddhibhoyar3@users.noreply.github.com>
2023-07-19 10:57:04 +0530

    Delete PolicyManagement_backend directory

--- PolicyManagement_backend/src/main/java/com/policymanagement/profile/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.policymanagement.profile.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
